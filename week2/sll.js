598eeadc3a7b45f14a197021961562e1928b1e81
dojo-json <jfranz@codingdojo.com>
2019-09-10 17:21:53 -0700

    linked list intro

---
+++ week2/sll.js
@@ -0,0 +1,114 @@
+
+
+class Node {
+  constructor(value) {
+    this.value = value
+    this.next = null;
+  }
+}
+
+class List {
+  constructor() {
+    this.head = null;
+  }
+
+  /** 
+   * Add Front
+Rudy isn’t nice: he cuts in line in front of everyone else. 
+Given a pointer to the first ListNode and a value, create a new node, assign it to the list head, 
+and return a pointer to the new head node.
+  */
+  addFront(value) {
+    const node = new Node(value);
+
+    node.next = this.head;
+
+    this.head = node;
+  }
+
+  /**  
+   * Remove Front
+Ha! Rudy is getting what he deserves – kicked out of line. 
+Given a pointer to the first node in a list, remove the head node and return the new list head node. 
+If the list is empty, return null. (return the value from the removed node)
+  */
+  removeFront() {
+    if (this.head === null) {
+      return null;
+    }
+
+    const node = this.head;
+
+    this.head = node.next;
+
+    return node.value;
+  }
+
+  /** 
+   * Front
+Finally, Tad and Sam reach the front of the line to get movie tickets. 
+Oh no – only one seat remains! Who was earlier in line: Tad or Sam? 
+Return the value (not the node) at the head of the list. 
+If the list is empty, return null.
+  */
+  front() {
+    if (this.head === null) {
+      return null;
+    }
+
+    return this.head.value;
+  }
+
+  /**  
+   * Contains
+Sam thinks Tad might be somewhere in a very long line waiting to attend the Superman movie. 
+Given a ListNode pointer and a val, return whether val is found in any node in the list.
+  */
+  contains(searchContent) {
+    let current = this.head;
+
+
+    while (current !== null) {
+      console.log('current is ', current);
+      if (current.value === searchContent) {
+        return true;
+      }
+
+      current = current.next;
+    }
+
+    return false;
+  }
+
+
+}
+
+
+const list = new List();
+
+
+// const node1 = new Node(10);
+// const node2 = new Node(15);
+// const node3 = new Node(20);
+
+// list.head = node1;
+
+// node1.next = node2;
+// node2.next = node3;
+
+// console.log(node1);
+// console.log(node2);
+// console.log(node3);
+list.addFront(10);
+list.addFront(15);
+list.addFront(20);
+console.log(list);
+
+// console.log(list.removeFront());
+
+console.log('----before front')
+console.log(console.log(list));
+console.log(list.front())
+console.log(list);
+
+console.log(list.contains(90));
