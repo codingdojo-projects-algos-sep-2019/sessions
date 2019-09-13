02615da0ef8b25625f5d67487c0bcaf935130979
dojo-json <jfranz@codingdojo.com>
2019-09-12 17:15:03 -0700

    linked lists with callbacks

--- week2/sll.js
+++ week2/sll.js
@@ -80,6 +80,98 @@ Given a ListNode pointer and a val, return whether val is found in any node in t
     return false;
   }
 
+  /**  
+   * July 20, 2013: about 5000 people wait in line for a chance to audition for American Idol. 
+   * Create a function that accepts a pointer to the first list node, and returns number of nodes in that SList.
+   * 
+  */
+  length() {
+    let count = 0;
+
+    this.traverse(function (node) {
+      count++;
+      console.log('current count is ' + count);
+    });
+
+    return count;
+  }
+
+  isEmpty() {
+    // (expression) ? (if true) : (if false);
+
+    return this.head === null;
+  }
+
+  traverse(callback) {
+    let current = this.head;
+
+    while (current !== null) {
+      // run function
+      callback(current);
+      current = current.next;
+    }
+  }
+
+
+  /**  
+   * Create display(node) for debugging that returns a string containing all list values. 
+   * Build what you wish console.log(myList) did!
+  */
+  display() {
+    console.log('pprinting list');
+
+    let current = this.head;
+
+    while (current !== null) {
+      console.log(`Node has value of ${current.value}`);
+      current = current.next;
+    }
+
+    console.log('list pprinting complete');
+  }
+
+  print() {
+    console.log('pprinting list');
+
+
+    this.traverse(function (node) {
+      console.log(`Node has value of ${node.value}`);
+    });
+
+    console.log('list pprinting complete');
+  }
+
+  /** 
+   * American Idol seems to air singers that are the best, and a few that seem like the worst! 
+   * Create function max(node) to return list’s largest val.
+  */
+  max() {
+    let largest = null;
+
+    this.traverse(function (cat) {
+      console.log('largest is ', largest);
+      if (cat.value > largest) {
+        largest = cat.value;
+      }
+    });
+
+    return largest;
+  }
+
+  /** 
+   * Create average(node) to return average val.
+  */
+
+  average() {
+    const length = this.length();
+    let sum = 0;
+    // this.traverse(function (node) {
+    //   sum += node.value;
+    // });
+    this.traverse(node => sum += node.value);
+
+    return this.isEmpty() ? null : sum / length;
+  }
 
 }
 
@@ -99,9 +191,10 @@ const list = new List();
 // console.log(node1);
 // console.log(node2);
 // console.log(node3);
-list.addFront(10);
-list.addFront(15);
-list.addFront(20);
+// list.addFront(10);
+// list.addFront(77);
+// list.addFront(15);
+// list.addFront(20);
 console.log(list);
 
 // console.log(list.removeFront());
@@ -112,3 +205,10 @@ console.log(list.front())
 console.log(list);
 
 console.log(list.contains(90));
+console.log(list.length());
+
+console.log(list.isEmpty());
+
+list.print();
+list.max();
+console.log(list.average());
